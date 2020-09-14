df66cd170c80a268a3679ff90319cf2c2666e30d
Harrisonmk <harrison.mitchell@hotmail.com>
2020-09-14 11:08:13 -0400

    project created

---
+++ backend/src/main/java/com/projeto/dspesquisaspringboot/DspesquisaspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.dspesquisaspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DspesquisaspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DspesquisaspringbootApplication.class, args);
+	}
+
+}
