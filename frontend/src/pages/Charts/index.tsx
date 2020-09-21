b32e9a6e9e41aac6fb1ac171db088be38bb738e0
Harrisonmk <harrison.mitchell@hotmail.com>
2020-09-20 21:06:02 -0400

    Configurando front-web para producao

--- frontend/src/pages/Charts/index.tsx
+++ frontend/src/pages/Charts/index.tsx
@@ -29,7 +29,8 @@ const initialPieData = {
 
 }
 
-const BASE_URL = 'http://localhost:8080'
+//const BASE_URL = 'http://localhost:8080'
+const BASE_URL = 'https://dspesquisa-springboot.herokuapp.com'
 
 const Charts = () => {
 
--- frontend/src/pages/Records/index.tsx
+++ frontend/src/pages/Records/index.tsx
@@ -6,7 +6,8 @@ import { formatDate } from './helpers';
 import Pagination from './Pagination';
 import Filters from '../../components/Filters';
 
-const BASE_URL = 'http://localhost:8080'
+//const BASE_URL = 'http://localhost:8080'
+const BASE_URL = 'https://dspesquisa-springboot.herokuapp.com'
 const Records = () => {
 
     const [recordsResponse, setRecordsResponse] = useState<RecordsResponse>();
